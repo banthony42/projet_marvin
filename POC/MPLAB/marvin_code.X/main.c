adadafb0a108674f9d0165604b81252ac431da9f
Labelec-vm <Labelec-vm>
2016-04-25 20:30:31 +0200

    Implementation du AnalogRead, mais probleme de compilation

--- POC/MPLAB/marvin_code.X/main.c
+++ POC/MPLAB/marvin_code.X/main.c
@@ -84,8 +84,7 @@ int main()
     allinone_IR_test();
     while (1)
     {
-
-        input_value = PORTBbits.RB5;
+        input_value = analogReadIR();
     }
     return (0);
 }
@@ -98,6 +97,8 @@ int main()
 void    allinone_IR_test(void)
 {
     sharp_pin_setup();
+    timer_4_setup();
+    PR4 = 10000;
 }
 
 
@@ -145,9 +146,37 @@ void    echo_setup()
  */
 void sharp_pin_setup()
 {
+    /*
+     * Configuration I/O
+     */
     LATBbits.LATB5 = 0;
     AD1PCFGbits.PCFG5 = 0; // RB5 en mode analogique
     TRISBbits.TRISB5 = 1; // RB5 en input
+    /*
+     * Configuration de l'ADC
+     */
+    AD1CHSbits.CH0SA = 5; // On selectionne le pin AN5 comme input positif du
+                          // multiplexer A.
+    AD1CHSbits.CH0NA = 0; // On selectionne Vr- comme tension de ref negative
+                          // du multiplexer A
+    AD1CON1bits.FORM = 0; // Formatage du resultat de la conversion en entier
+                          // non-signe sur 16 bits (10 bits actifs)
+    AD1CON1bits.SSRC = 0; // Samle clock source en mode manuel : il faudra clear
+                          // le bit SAMP pour arreter le sample et initier la
+                          // conversion
+    AD1CON2bits.VCFG = 0; // On selectionne AVDD et AVSS comme reference de
+                          // tension pour Vr+ et Vr-
+    AD1CON2bits.CSCNA = 0; // Ne pas scanner les inputs (car on ne lit qu'un seul
+                          // pin, AN5)
+    AD1CON2bits.SMPI = 0; // 1 conversion par interrupt
+    AD1CON2bits.BUFM = 0; // On replit le buffer avec les 16 bits d'un seul coup
+    AD1CON2bits.ALTS = 0; // On utilise uniquement le multiplexer A
+    AD1CON3bits.ADRC = 0; // Selection de PBCLK comme clock de conversion
+    AD1CON3bits.ADCS = 0; // Prescaler de 2 pour cette clock. TAD = TPB / 2
+                          // Donc TAD = 0.5MHz
+    AD1CON3bits.SAMC = 1; // Sample time de 1 TAD. Ce n'est peut-etre pas utile,
+                          // car en principe nous sommes en sampling manuel
+    AD1CON1bits.ON = 1;   // On active le module ADC
 }
 
 void button_setup()
@@ -177,7 +206,7 @@ void timer_4_setup(void)
     T4CONbits.TGATE = 0; // Ignore
     T4CONbits.TCKPS = 4; // Prescaler a 16
     TMR4 = 0; // Reset du timer
-    PR4 = 200; // Pour obtenir une periode de 1 seconde
+    PR4 = 200; // Pour obtenir une periode
     T4CONbits.ON = 1; // On active le timer 4
 }
 
@@ -242,4 +271,19 @@ void trigger_setup(void)
 {
     LATEbits.LATE0 = 0; // Pin trigger a 0
     TRISEbits.TRISE0 = 0; // output
+}
+
+
+/*
+ * Lecture de la tension du Sharp IR via l'utilisation de l'ADC en mode manuel
+ */
+u16 analogReadIR(void)
+{
+    AD1CON1bits.SAMP = 1; // On lance le sampling
+    TMR4 = 0;
+    while (TMR4 < 6250) // On attend 100 ms
+        ;
+    AD1CON1bits.SAMP = 0; // On lance la conversion
+    while (!(AD1CON1 && 0x0001)); // On attend la fin de la conversion
+    return (ADC1BUF0);
 }
