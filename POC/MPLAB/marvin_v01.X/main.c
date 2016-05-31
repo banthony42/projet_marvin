0d819afe9b220652dc84a15b453a0235ccfcb3f8
Labelec-vm <Labelec-vm>
2016-05-31 11:15:27 +0200

    configsbits

--- POC/MPLAB/marvin_v01.X/main.c
+++ POC/MPLAB/marvin_v01.X/main.c
@@ -1,3 +1,12 @@
+/*
+ * Auteur : abary, amulin, banthony, tbelmaison
+ * Premiere Version du projet Marvin sur le PIC32MX340F512H
+ * 
+ */
+
+
+#include <p32xxxx.h>
+#include <sys/attribs.h>
 #include "types.h"
 
 
